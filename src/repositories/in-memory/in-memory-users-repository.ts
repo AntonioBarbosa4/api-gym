b1c3008285931954e70ece45321fffc1e109b077
Antonio Francisco <afpb04@hotmail.com>
2023-03-20 11:13:27 -0300

    test: :test_tube: config vitest and firsts tests

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,28 @@
+import { Prisma, User } from '@prisma/client';
+import { IUsersRepository } from '../users-repository';
+import { randomUUID } from 'crypto';
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find((user) => user.email === email);
+
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.users.push(user);
+    return user;
+  }
+}
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,11 +1,15 @@
 import { IUsersRepository } from '@/repositories/users-repository';
 import { hash } from 'bcryptjs';
 import { UserAlreadyExistsError } from './errors/user-already-exists-error';
+import { User } from '@prisma/client';
 
 interface IRegisterUseCaseRequest {
   name: string;
   email: string;
   password: string;
+}
+interface IRegisterUseCaseResponse {
+  user: User;
 }
 
 export class RegisterUseCase {
@@ -15,7 +19,7 @@ export class RegisterUseCase {
     name,
     email,
     password,
-  }: IRegisterUseCaseRequest): Promise<void> {
+  }: IRegisterUseCaseRequest): Promise<IRegisterUseCaseResponse> {
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
     if (userWithSameEmail) {
@@ -24,6 +28,12 @@ export class RegisterUseCase {
 
     const password_hash = await hash(password, 8);
 
-    await this.usersRepository.create({ name, email, password_hash });
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return { user };
   }
 }
