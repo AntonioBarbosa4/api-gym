43e809c687eabdccbb894a53e380e0ffb55824cc
Antonio Francisco <afpb04@hotmail.com>
2023-04-04 14:36:53 -0300

    feat: :sparkles: add get user profile use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -5,6 +5,15 @@ import { randomUUID } from 'crypto';
 export class InMemoryUsersRepository implements IUsersRepository {
   private users: User[] = [];
 
+  async findById(id: string): Promise<User | null> {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     const user = this.users.find((user) => user.email === email);
 
---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,27 @@
+import { IUsersRepository } from '@/repositories/users-repository';
+import { User } from '@prisma/client';
+import { ResourceNotFountError } from './errors/resource-not-found-error';
+
+interface IGetUserProfileUseCaseRequest {
+  userId: string;
+}
+
+interface IGetUserProfileUseCaseResponse {
+  user: User;
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({
+    userId,
+  }: IGetUserProfileUseCaseRequest): Promise<IGetUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new ResourceNotFountError();
+    }
+
+    return { user };
+  }
+}
