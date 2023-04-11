3f0ebf38baa367a2d23a03d13c5dc6b86ca3c9a5
Antonio Francisco <afpb04@hotmail.com>
2023-04-11 17:34:24 -0300

    feat: :sparkles: create gym controller

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case';
+
+export async function create(
+  request: FastifyRequest,
+  replay: FastifyReply
+): Promise<FastifyReply> {
+  const createGymBodySchema = z.object({
+    title: z.string(),
+    description: z.string().nullable(),
+    phone: z.string().nullable(),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { title, description, phone, latitude, longitude } =
+    createGymBodySchema.parse(request.body);
+
+  const CreateGymUseCase = makeCreateGymUseCase();
+  await CreateGymUseCase.execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  });
+
+  return replay.status(201).send();
+}
