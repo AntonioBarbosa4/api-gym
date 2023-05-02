3df75cf4cd6f8416a956f858158197805e732b09
Antonio Francisco <afpb04@hotmail.com>
2023-05-02 14:17:17 -0300

    feat: :sparkles: refresh token and rbac

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -20,14 +20,35 @@ export async function authenticate(
     const { user } = await authenticateUseCase.execute({ email, password });
 
     const token = await replay.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
         },
       }
     );
-    return replay.status(200).send({ token });
+    const refreshToken = await replay.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      }
+    );
+    return replay
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return replay.status(400).send({ message: err.message });
